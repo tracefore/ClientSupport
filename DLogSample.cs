708bcdf90e16c5603c0a5a9dca934aaaf9897f0f
shunaaa <shunaaa@yahoo.com>
2015-07-23 14:19:18 +0800

    Add dlog sample

---
+++ DLogSample.cs
@@ -0,0 +1,25 @@
+ // Prepare content
+            string query = "<QueryXml>Sample Query</<QueryXml>>";
+            string reply = "<ReplyXml>Sample Reply</<ReplyXml>>";
+
+            // Prepare token and ID
+            string token = Guid.NewGuid().ToString();
+            string sessionId = "123456789";
+            string seqnum = "1";
+
+            // Dlog
+            DLogManager log = new DLogManager();
+            log.TargetFolder = "your/target/path";
+
+            // Prepare path and service
+            string servicename = DLogManager.GetServiceName(query);
+            string path = log.FullPath(token, sessionId, seqnum, servicename);
+
+            // Save Query
+            DLogManager.Save(path, query);
+
+            // Save Reply
+            servicename = DLogManager.GetServiceName(reply);
+            path = log.FullPath(token, sessionId, seqnum, servicename);
+
+            DLogManager.Save(path, reply);
